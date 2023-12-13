7f450588043316f49702d733d2df4366036720a7
Vitor Sofiati <vitor.sofiati@live.com>
2023-12-13 12:17:42 -0300

    alterações de design e acessbilidade

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("Teste");
+
+// const iconeContato = document.querySelector(".footer-icone");
+// const pathContato = iconeContato.querySelector("path");
+// console.log(pathContato);
+// pathContato.style.fill = "#000";

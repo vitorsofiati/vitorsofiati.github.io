cd10b6c1e84886e6c5aad588f737b3516265e7f1
Vitor Sofiati <vitor.sofiati@live.com>
2023-12-19 17:45:13 -0300

    Criação do index e script de redirecionamento para os idiomas. Alterações no layout e textos.

---
+++ js/idioma.js
@@ -0,0 +1,10 @@
+// const idiomaUsuario = navigator.language;
+// if (idiomaUsuario == "pt-BR") {
+
+//   window.document.location.href = "./portugues/home-pt.html";
+// } else {
+//   window.document.location.href = "./ingles/home-en.html";
+// }
+
+
+

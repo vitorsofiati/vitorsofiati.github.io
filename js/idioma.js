5113d25a591041119186da52fde666f7e607668b
Vitor Sofiati <vitor.sofiati@live.com>
2023-12-27 12:19:54 -0300

    Ajustes em texto e responsividade da página em inglês. Adicionado Português de Portugual no script de idioma. Adicionadas thumbnails. Alteração de foto de perfil

--- js/idioma.js
+++ js/idioma.js
@@ -1,7 +1,7 @@
 // Redireciona o usuário para a página específica baseado na linguagem do navegador
 
 const idiomaUsuario = navigator.language;
-if (idiomaUsuario == "pt-BR") {
+if (idiomaUsuario == "pt-BR"||"pt") {
 
   window.document.location.href = "./portugues/home-pt.html";
 } else {

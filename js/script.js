07e02c7aff2297c6819dab53ce2bad12e8e11eac
unknown <vitor.sofiati@live.com>
2024-01-30 17:46:15 -0300

    reestruturação do script de animação + criação do script de highlight  do link interno ativo + pequenas  alterações de conteúdo e layout

--- js/script.js
+++ js/script.js
@@ -1,28 +1,71 @@
 document.documentElement.classList.add("js");
-
-const formacaoSubtitulo = document.querySelector(".formacao-subtitulo");
-const projetosSubtitulo = document.querySelector(".projetos-subtitulo");
-
-// executar plugin de animação 
 new SimpleAnime();
 
+function initEventosScroll() {
+  const elementosAnimados = document.querySelectorAll('.js-anima');
+  const linksInternos = document.querySelectorAll('nav a[href^="#"]');
+  const sections = document.querySelectorAll('section');
 
-// a classe anime era atribuída logo no início pelo plugin SimpleAnime, removi essa parte no arquivo do plugin.
-function animarFormacao() {
-  let posicaoFormacao = formacaoSubtitulo.getBoundingClientRect().top;
-  if (posicaoFormacao < 600) {
-    formacaoSubtitulo.classList.add("anime");
-    window.removeEventListener("scroll", animarFormacao);
+if(sections.length){  
+  const janelaParcialNav = window.innerHeight * 0.49;
+
+  function navHighlight() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionBottom = section.getBoundingClientRect().bottom;
+      const sectionVisivel = (janelaParcialNav - sectionTop > 0) && (janelaParcialNav - sectionBottom < 0);
+
+      if (sectionVisivel) {
+        const sectionId = section.getAttribute('id');
+        const linkAtivo = document.querySelector(`a[href="#${sectionId}"]`);
+        const liPai = linkAtivo.parentElement;
+
+        if (linkAtivo) {
+          linkAtivo.classList.add('ativo');
+          liPai.classList.add('ativo');
+        }
+      } else {
+          const sectionId = section.getAttribute('id');
+          const linkAtivo = document.querySelector(`a[href="#${sectionId}"]`);
+          const liPai = linkAtivo.parentElement;
+
+          if (linkAtivo) {
+            linkAtivo.classList.remove('ativo');
+            liPai.classList.remove('ativo');
+
+          }
+      }
+    });
   }
 }
 
-function animarProjetos() {
-  let posicaoProjetos = projetosSubtitulo.getBoundingClientRect().top;
-  if (posicaoProjetos <= 700) {
-    projetosSubtitulo.classList.add("anime");
-    window.removeEventListener("scroll", animarProjetos);
-  }
-}
+navHighlight();
+window.addEventListener('scroll', navHighlight);
 
-window.addEventListener("scroll", animarFormacao);
-window.addEventListener("scroll", animarProjetos);
+
+  if (elementosAnimados.length){
+    const janelaParcialAnimacao = window.innerHeight * .8; 
+
+    let jaForamAnimados = 0;
+    
+    function animarElementos() {
+
+      elementosAnimados.forEach((elemento) => {        
+        const elementoTop = elemento.getBoundingClientRect().top; 
+        const elementoVisivel = (janelaParcialAnimacao - elementoTop > 0);
+
+        if (elementoVisivel && !elemento.classList.contains('anime')){
+          elemento.classList.add('anime');
+          jaForamAnimados++;
+        }
+      })
+
+      if(jaForamAnimados === elementosAnimados.length){
+        window.removeEventListener('scroll',animarElementos);
+      }
+    }
+    window.addEventListener('scroll', animarElementos);
+  }
+
+}
+initEventosScroll();

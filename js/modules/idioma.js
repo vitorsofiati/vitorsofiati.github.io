c7e8ec10d0e49e00f398b288f47054977c333e07
Vitor <vitor.sofiati@live.com>
2024-03-26 14:10:52 -0300

    Instalação de pacotes NPM - Eslint

---
+++ js/modules/idioma.js
@@ -0,0 +1,8 @@
+// Redireciona o usuário para a página específica baseado na linguagem do navegador
+
+const idiomaUsuario = navigator.language;
+if (idiomaUsuario == 'pt-BR' || 'pt') {
+	window.document.location.href = './portugues/home-pt.html';
+} else {
+	window.document.location.href = './ingles/home-en.html';
+}
